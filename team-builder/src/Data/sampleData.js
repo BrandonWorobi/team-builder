1e1202d2330f7522370ccceea8d50e749012ab63
BrandonWorobi <79031043+BrandonWorobi@users.noreply.github.com>
2021-07-19 22:22:38 -0400

    Added Sample Data, Form Component & Added State to App

---
+++ team-builder/src/Data/sampleData.js
@@ -0,0 +1,36 @@
+import { v4 as uuid } from 'uuid';
+
+const sampleData = [
+{
+	id: uuid(),
+	name: 'Col Rhombus',
+	email: 'ColonelRhombus@usarmy.gov',
+	role: 'CIA Ops Trainer',
+},
+{
+	id: uuid(),
+	name: 'Emmett Fitz-Hume',
+	email: 'Emmett.Fitz-Hume@DOS.gov',
+	role: 'Envoy',
+},
+{
+	id: uuid(),
+	name: 'Austin Millbarge',
+	email: 'austinmillbarge@dod.gov',
+	role: 'code breaker',
+},
+{
+	id: uuid(),
+	name: 'Mr. Ruby',
+	email: 'mrruby@dia.gov',
+	role: 'Defense Intelligence Agency',
+},
+{
+	id: uuid(),
+	name: 'Mr. Keyes'
+	email: 'mrkeyes@dia.gov'
+	role: 'Defense Intelligence Agency'
+  }
+];
+
+export default sampleData; 
